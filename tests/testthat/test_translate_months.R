65b846d0e915c6da72566629fb9f5a3abbe22dd5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-18 15:48:12 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_translate_months.R
+++ tests/testthat/test_translate_months.R
@@ -1,5 +1,9 @@
 describe("Prueba cambio de formato en fecha", {
   it("spanish_to_iso", {
+    date_in_spanish <- "1/Abr/2023"
+    obtained_date <- spanish_to_iso(date_in_spanish)
+    expected_date <- "2023-04-01"
+    expect_equal(obtained_date, expected_date)
     date_in_spanish <- "18/Ene/2023"
     obtained_date <- spanish_to_iso(date_in_spanish)
     expected_date <- "2023-01-18"
