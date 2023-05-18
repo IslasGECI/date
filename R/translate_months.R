9d0a2fd52b0c98e3c5b897044cec381924372cbc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-18 15:56:40 -0700

    ✅ Pass tests

--- R/translate_months.R
+++ R/translate_months.R
@@ -1,5 +1,6 @@
 spanish_to_iso <- function(spanish_date) {
   english_date <- stringr::str_replace(spanish_date, "Ene", "Jan")
   english_date <- stringr::str_replace(english_date, "Abr", "Apr")
+  english_date <- stringr::str_replace(english_date, "Ago", "Aug")
   as.Date(english_date, "%d/%b/%Y") %>% format("%Y-%m-%d")
 }
